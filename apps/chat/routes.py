66659a70755da11b510eca1130b4d153c0f0bfc1
tubehao <guanhao2022@sjtu.edu.cn>
2024-07-24 20:02:26 +0800

    tried to make node label visible but failed.

--- apps/chat/routes.py
+++ apps/chat/routes.py
@@ -22,9 +22,9 @@ def get_response():
     user_message = data['message']
     
     # 使用初始化后的模型
-    pipeline_prompt = current_app.config['MODEL_PIPELINE']
-    pipeline_chat = current_app.config['MODEL_SOLUTION']
-    pipeline_pure = current_app.config['MODEL_PURE']
+    # pipeline_prompt = current_app.config['MODEL_PIPELINE']
+    # pipeline_chat = current_app.config['MODEL_SOLUTION']
+    # pipeline_pure = current_app.config['MODEL_PURE']
     
     current_database = current_app.config.get('CURRENT_DATABASE', 'yago')
     query_prompt = current_app.config['QUERY_PROMPT'][current_database]
@@ -33,17 +33,18 @@ def get_response():
     prompt = f"Our dataset contains a {query_prompt['type']} stored in a Neo4j database. With node including {query_prompt['label']} and uri. Translate the following natural language query into a Cypher query for Neo4j and wrap the query with '```' \n \n for example,{query_prompt['example']}.\nNatural Language Query: {user_message}\n\n\n\nCypher Query:"
     print(prompt)
     # 调用模型生成输出
-    output = pipeline_prompt(prompt, max_new_tokens=100)
-    generated_text = output[0]['generated_text'].strip()
+    # output = pipeline_prompt(prompt, max_new_tokens=100)
+    # generated_text = output[0]['generated_text'].strip()
     
-    # 使用正则表达式提取用```包裹的Cypher查询
-    match = re.search(r'```(.*?)```', generated_text[len(prompt):], re.DOTALL)
-    if match:
-        cypher_query = match.group(1).strip()
-        print("~~~~~~~~~~~~~~~~~~~~~")
-        print(cypher_query)
-    else:
-        cypher_query = "MATCH (n) RETURN n LIMIT 1"  # 默认查询语句，如果没有匹配到
+    # # 使用正则表达式提取用```包裹的Cypher查询
+    # match = re.search(r'```(.*?)```', generated_text[len(prompt):], re.DOTALL)
+    # if match:
+    #     cypher_query = match.group(1).strip()
+    #     print("~~~~~~~~~~~~~~~~~~~~~")
+    #     print(cypher_query)
+    # else:
+    #     cypher_query = "MATCH (n) RETURN n LIMIT 1"  # 默认查询语句，如果没有匹配到
+    cypher_query = "MATCH (n)-[r]-(neighbor) WHERE n.rdfs__label CONTAINS 'Basketball' RETURN n, r, neighbor LIMIT 25"
 
     # 使用生成的Cypher查询语句在Neo4j中查询
     driver = current_app.config['NEO4J_DRIVER']
@@ -58,17 +59,17 @@ def get_response():
     # 构建提示以让LLM解决问题
     result_prompt = f"Knowledge: {graph_data}. Use the knowledge to address the following question. Don't contain it in your answer directly, you can just refer to it. \n\n Question: {user_message}. \n Answer:"
 
-    final_output = pipeline_chat(result_prompt, max_new_tokens=200)
-    solution = final_output[0]['generated_text'].strip()
+    # final_output = pipeline_chat(result_prompt, max_new_tokens=200)
+    # solution = final_output[0]['generated_text'].strip()
 
     pure_prompt = f"Address the following question. \n\n Question:{user_message}.Answer:"
-    pure_output = pipeline_pure(pure_prompt, max_new_tokens=200)
-    pure_solution = pure_output[0]['generated_text'].strip()
+    # pure_output = pipeline_pure(pure_prompt, max_new_tokens=200)
+    # pure_solution = pure_output[0]['generated_text'].strip()
     
     # 返回解决方案
     return jsonify({
-        "solution_part1": solution[len(result_prompt):],  # 第一部分
-        "solution_part2": pure_solution[len(pure_prompt):],  # 第二部分
+        "solution_part1": "1111",#solution[len(result_prompt):],  # 第一部分
+        "solution_part2": '1111', #pure_solution[len(pure_prompt):],  # 第二部分
         "graph_data": graph_data  # 返回图表数据
     })
 
@@ -91,6 +92,7 @@ def parse_neo4j_results(results):
             "id": node_uri,
             "uri": node_uri,
             "label": node_label,
+            "data": node_label,
             "comment": node_comment,
             "year_of_publication": node_year_of_publication,
             "published_in": node_published_in,
@@ -122,7 +124,7 @@ def parse_neo4j_results(results):
             graph_data['edges'].append({
                 "source": node_uri, 
                 "target": neighbor_uri,
-                "type": relationship[1]  # 假设relationship是一个三元组 (start_node, relationship_type, end_node)
+                # "type": relationship[1]  # 假设relationship是一个三元组 (start_node, relationship_type, end_node)
             })
 
     return graph_data
